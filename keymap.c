06ed876ae4cf326d1ad047f1d0729e453d68c86c
e0a6 <76806725+e0a6@users.noreply.github.com>
2021-09-17 19:41:09 -0600

    Changed the default media layer to my own custom layer
    The config came with a default layer called "_MEDIA", I changed this
    layer to my own custom version.

--- keymap.c
+++ keymap.c
@@ -1,15 +1,15 @@
 #include QMK_KEYBOARD_H
 
 #define _DVORAK 0 // Base Dvorak layer
-#define _MEDIA 1  // Media layer
+#define _CUSTOM 1 // My Custom layer
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_DVORAK] = LAYOUT(
         // left hand
-        KC_ESC,   KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5, KC_F6, KC_F7, KC_F8,
+        RESET,    KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5, KC_F6, KC_F7, KC_F8,
         KC_EQL,   KC_1,    KC_2,    KC_3,    KC_4,   KC_5,
         KC_ESC,   KC_QUOT, KC_COMM, KC_DOT,  KC_P,   KC_Y,
-        KC_TAB,  KC_A,    KC_O,    KC_E,    KC_U,   KC_I,
+        KC_TAB,   KC_A,    KC_O,    KC_E,    KC_U,   KC_I,
         KC_LSFT,  KC_SCLN, KC_Q,    KC_J,    KC_K,   KC_X,
                   KC_GRV,  KC_INS, KC_LEFT, KC_RIGHT,
         // left thumb
@@ -26,17 +26,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // right thumb
         KC_RGUI,    KC_RCTL,
         KC_RALT,
-        TG(_MEDIA), KC_ENT, KC_SPC
+        KC_NO, KC_ENT, KC_SPC
     ),
 
-[_MEDIA] = LAYOUT(
+[_CUSTOM] = LAYOUT(
        // left hand
        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO,
        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-       KC_NO,   KC_BTN4, KC_BTN3, KC_BTN2, KC_BTN1, KC_NO,
-       KC_NO,   KC_NO,   KC_MUTE, KC_VOLD, KC_VOLU, KC_NO,
-                KC_NO,   KC_NO,   KC_MS_D, KC_MS_U,
+       KC_ESC,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,
+       KC_EQL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,
+       KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,
+                KC_GRV,  KC_INS,  KC_LEFT, KC_RIGHT,
         // left thumb
                                            KC_NO,   KC_NO,
                                                     KC_NO,
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_POWER,
        KC_NO,   KC_VOLU, KC_MS_U, KC_VOLD, KC_NO,   KC_NO,
-       KC_NO,   KC_MS_L, KC_MS_D, KC_MS_R, KC_NO,   KC_NO,
+       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
        KC_NO,   KC_MPRV, KC_MPLY, KC_MNXT, KC_NO,   KC_NO,
                 KC_MS_L, KC_MS_R, KC_NO,   KC_NO,
        // right thumb
