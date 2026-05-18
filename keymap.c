aae53088dc903e76c9bb80b2088e8ce65c2a25c2
ubuntu <76806725+e0a6@users.noreply.github.com>
2026-05-18 02:30:51 +0000

    Port keymap to modern QMK for kinT (kint36) controller
    - Re-order LAYOUT() args to match kint36's row-by-row macro
    - Replace deprecated keycodes: RESET→QK_BOOT, KC_SLCK→KC_SCRL,
      KC_POWER→KC_PWR, KC_MS_U/L/R→MS_UP/LEFT/RGHT
    - Remove obsolete TMK stubs (action_get_macro, matrix_init_user,
      matrix_scan_user, led_set_user)
    - Disable _CUSTOM layer activation (KC_FN0 slot → KC_NO)
    - Bind HYPR_T(KC_ENT) on right thumb middle

--- keymap.c
+++ keymap.c
@@ -5,86 +5,52 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_DVORAK] = LAYOUT(
-        // left hand
-        RESET,    KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5, KC_F6, KC_F7, KC_F8,
-        KC_EQL,   KC_1,    KC_2,    KC_3,    KC_4,   KC_5,
-        KC_ESC,   KC_QUOT, KC_COMM, KC_DOT,  KC_P,   KC_Y,
-        KC_TAB,   KC_A,    KC_O,    KC_E,    KC_U,   KC_I,
-        KC_LSFT,  KC_SCLN, KC_Q,    KC_J,    KC_K,   KC_X,
-                  KC_GRV,  KC_INS, KC_LEFT, KC_RIGHT,
-        // left thumb
-                                            KC_NO, KC_LCTL,
-                                     KC_PGUP,
-                          RCTL_T(KC_BSPC), KC_DEL, KC_PGDN,
-        // right hand
-        KC_F9,  KC_F10,   KC_F11,   KC_F12,   KC_PSCR, KC_SLCK, KC_PAUS, KC_FN0, KC_1,
-        KC_6,   KC_7,     KC_8,     KC_9,     KC_0,    KC_MINS,
-        KC_F,   KC_G,     KC_C,     KC_R,     KC_L,    KC_SLSH,
-        KC_D,   KC_H,     KC_T,     KC_N,     KC_S,    KC_BSLS,
-        KC_B,   KC_M,     KC_W,     KC_V,     KC_Z,    KC_RSFT,
-                          KC_UP,  KC_DOWN, KC_LBRC, KC_RBRC,
-        // right thumb
-        KC_RGUI, RGUI_T(KC_ESC),
-        KC_RALT,
-        KC_END, KC_ENT, RALT_T(KC_SPC)
+        // top function row: left hand                          | right hand
+        QK_BOOT, KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5, KC_F6, KC_F7, KC_F8,        KC_F9,  KC_F10,  KC_F11,   KC_F12,   KC_PSCR, KC_SCRL, KC_PAUS, KC_NO,       KC_1,
+        // number row
+        KC_EQL,  KC_1,    KC_2,    KC_3,    KC_4,   KC_5,                               KC_6,   KC_7,    KC_8,     KC_9,     KC_0,    KC_MINS,
+        // top alpha row
+        KC_ESC,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,   KC_Y,                               KC_F,   KC_G,    KC_C,     KC_R,     KC_L,    KC_SLSH,
+        // home row
+        KC_TAB,  KC_A,    KC_O,    KC_E,    KC_U,   KC_I,                               KC_D,   KC_H,    KC_T,     KC_N,     KC_S,    KC_BSLS,
+        // bottom alpha row
+        KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,   KC_X,                               KC_B,   KC_M,    KC_W,     KC_V,     KC_Z,    KC_RSFT,
+        // arrow row
+                 KC_GRV,  KC_INS,  KC_LEFT, KC_RIGHT,                                           KC_UP,   KC_DOWN,  KC_LBRC,  KC_RBRC,
+
+        // thumb clusters: top row (left pair, right pair)
+                                            KC_NO,           KC_LCTL,        KC_RGUI,  RGUI_T(KC_ESC),
+        // thumb clusters: middle row (left, right)
+                                                             KC_PGUP,        KC_RALT,
+        // thumb clusters: bottom row (left triple, right triple)
+                                  RCTL_T(KC_BSPC), KC_DEL,   KC_PGDN,        KC_END,   HYPR_T(KC_ENT), RALT_T(KC_SPC)
     ),
 
 [_CUSTOM] = LAYOUT(
-       // left hand
-       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO,
-       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-       KC_ESC,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,
-       KC_EQL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,
-       KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,
-                KC_GRV,  KC_INS,  KC_LEFT, KC_RIGHT,
-        // left thumb
-                                           KC_NO,   KC_NO,
-                                                    KC_NO,
-                                  KC_NO,   KC_NO,   KC_TRNS,
-       // right hand
-       KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_POWER,
-       KC_NO,   KC_VOLU, KC_MS_U, KC_VOLD, KC_NO,   KC_NO,
-       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
-       KC_NO,   KC_MPRV, KC_MPLY, KC_MNXT, KC_NO,   KC_NO,
-                KC_MS_L, KC_MS_R, KC_NO,   KC_NO,
-       // right thumb
-       KC_NO, KC_NO,
-       KC_NO,
-       KC_END, KC_ENT, RSFT_T(KC_SPC)
+        // top function row
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO, KC_NO, KC_NO, KC_NO,         KC_NO,  KC_NO,   KC_NO,    KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_TRNS,     KC_NO,
+        // number row
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  KC_NO,                              KC_NO,  KC_NO,   KC_NO,    KC_NO,    KC_NO,   KC_PWR,
+        // top alpha row (mirrors base)
+        KC_ESC,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,   KC_Y,                               KC_NO,  KC_VOLU, MS_UP,    KC_VOLD,  KC_NO,   KC_NO,
+        // home row (numbers on left)
+        KC_EQL,  KC_1,    KC_2,    KC_3,    KC_4,   KC_5,                               KC_6,   KC_7,    KC_8,     KC_9,     KC_0,    KC_MINS,
+        // bottom alpha row
+        KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,   KC_X,                               KC_NO,  KC_MPRV, KC_MPLY,  KC_MNXT,  KC_NO,   KC_NO,
+        // arrow row
+                 KC_GRV,  KC_INS,  KC_LEFT, KC_RIGHT,                                           MS_LEFT, MS_RGHT,  KC_NO,    KC_NO,
+
+        // thumb clusters: top row
+                                            KC_NO,           KC_NO,          KC_NO,    KC_NO,
+        // thumb clusters: middle row
+                                                             KC_NO,          KC_NO,
+        // thumb clusters: bottom row
+                                  KC_NO,   KC_NO,   KC_TRNS,                 KC_END,   KC_ENT, RSFT_T(KC_SPC)
 )
 };
 
-const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
-{
-  // MACRODOWN only works in this function
-      switch(id) {
-        case 0:
-          if (record->event.pressed) {
-            register_code(KC_RSFT);
-          } else {
-            unregister_code(KC_RSFT);
-          }
-        break;
-      }
-    return MACRO_NONE;
-};
-
-
-void matrix_init_user(void) {
-
-}
-
-void matrix_scan_user(void) {
-
-}
-
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   return true;
-}
-
-void led_set_user(uint8_t usb_led) {
-
 }
 
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
